59cddcb56f3bb7534319e6e6da97ce8f3842c11e
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-09 14:35:08 +0100

    working on the if inside the cycle for

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,4 +2,9 @@ const boxNumberHtml= document.getElementById(boxNumber)
 
 for (let i=0 ; i<=100; i++){
     console.log(i)
+
+    const Element =`<div class="col">${i}</div>`
+    boxNumber.innerHTML+=Element
+
+    if(){}
 }
