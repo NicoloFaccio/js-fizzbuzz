17c563c49ef8c28276df828274ffa0ff4af01520
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-09 16:18:24 +0100

    fixed the if section adding  the differents alse if

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,19 @@
 const boxNumberHtml= document.getElementById(boxNumber)
 
-for (let i=0 ; i<=100; i++){
+for (let i=1 ; i<=100; i++){
     console.log(i)
-
-    const Element =`<div class="col">${i}</div>`
-    boxNumber.innerHTML+=Element
 
     if( (i % 3)===0 ){
         const Element ="<div>Fizz</div>"
         boxNumber.innerHTML+=Element
-    }     
+    } else if ( (i % 5)===0 ) {    
+        const Element ="<div>Buzz</div>"
+        boxNumber.innerHTML+=Element
+    } else if ( (i % 15)===0 ) {    
+        const Element ="<div>FizzBuzz</div>"
+        boxNumber.innerHTML+=Element
+    } else {
+        const Element =`<div class="col">${i}</div>`
+        boxNumber.innerHTML+=Element
+    }   
 }
