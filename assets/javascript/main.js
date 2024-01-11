b0eeeb3172095f3e401f8067832ceac937f61fea
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-11 18:19:06 +0100

    fixed the element of the if

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,16 +1,16 @@
-const boxNumberHtml= document.getElementById(boxNumber)
+const boxNumberHtml= document.getElementById("boxNumber")
 
 for (let i=1 ; i<=100; i++){
     console.log(i)
 
-    if( (i % 3)===0 ){
-        const Element ="<div>Fizz</div>"
+    if( (i % 3)===0 && (i % 5)===0 ){
+        const Element ="<div>FizzBuzz</div>"
         boxNumber.innerHTML+=Element
     } else if ( (i % 5)===0 ) {    
         const Element ="<div>Buzz</div>"
         boxNumber.innerHTML+=Element
-    } else if ( ((i % 3)===0) && ((i % 5)===0) ) {    
-        const Element ="<div>FizzBuzz</div>"
+    } else if ( (i % 3)===0 ) {    
+        const Element ="<div>Fizz</div>"
         boxNumber.innerHTML+=Element
     } else {
         const Element =`<div class="col">${i}</div>`
